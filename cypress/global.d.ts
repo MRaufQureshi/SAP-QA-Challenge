f359f59c5581f4b6d705b89b0d6ee19329a0867b
MRaufQureshi <mrauf.q@gmail.com>
2024-03-14 12:49:41 +0100

    02 ✍️ Added global types for cypress

---
+++ cypress/global.d.ts
@@ -0,0 +1,41 @@
+// <reference types="cypress" />
+declare namespace Cypress {
+
+  type LoginOptions = {
+    rememberUser: boolean;
+  };
+
+  interface Chainable {
+    /**
+     * Open a url
+     * @param {string} path
+     */
+    open(path: string):Cypress.Chainable<Cypress.AUTWindow>
+
+    /**
+     * Logs-in user by using UI
+     */
+    login(username: string, password: string, loginOptions?: LoginOptions): void;
+
+    /**
+     * Logs out via UI by triggering logout event
+     */
+    logoutViaUI(): void;
+
+    /**
+     * Custom command to select elements
+     */
+    getBySel(dataTestAttribute: string, args?: any): Chainable<JQuery<HTMLElement>>;
+
+    /**
+     * Custom command to make snapshots with full name formed from the test title + suffix easier
+     */
+    visualSnapshot(maybeName?): Chainable<any>;
+
+    /**
+     * Logs-in user by using API request
+     * loginByApi(username: string, password?: string): Chainable<Response>;
+     */
+
+  }
+}
