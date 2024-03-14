a6d65fb2e7f3d2ad869121b1164b708f52d15267
MRaufQureshi <mrauf.q@gmail.com>
2024-03-14 21:38:46 +0100

    03 ✍️ Add search selectors

---
+++ cypress/objects/ShoppingCartSelector.ts
@@ -0,0 +1,11 @@
+export const Shopping_Cart = {
+    columnDescription: '.cart_description',
+    productName: '.product-name > a',
+
+}
+
+export class ShoppingCart{
+    public static x(){
+
+    }
+}
